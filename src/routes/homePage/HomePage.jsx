6d4b63fe586ecebdb5ef603597abe3eb35236edd
APayes <apayes@cdvi.ca>
2024-07-28 12:16:55 -0400

    React Typing Effect Animation; 44:54

--- src/routes/homePage/HomePage.jsx
+++ src/routes/homePage/HomePage.jsx
@@ -1,12 +1,31 @@
 import { Link } from 'react-router-dom'
+
 import './homePage.css'
 
 const HomePage = () => {
   return (
     <div className="homepage">
-      <Link to='/dashboard'>
-        Home
-      </Link>
+      <img src="/orbital.png" alt="" className='orbital' />
+
+      <div className="left">
+        <h1>AI CHAT</h1>
+        <h2>Supercharge your creativity and productivity</h2>
+        <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, magnam sit repellendus voluptas nam eligendi at. Officiis, deserunt velit! Quia a suscipit aliquid iste dolor.</h3>
+
+        <Link to='/dashboard'>Get Started</Link>
+      </div>
+
+      <div className="right">
+        <div className="imgContainer">
+          <div className="bgContainer">
+            <div className="bg">
+
+            </div>
+          </div>
+
+          <img src="/bot.png" alt="" className='bot' />
+        </div>
+      </div>
     </div>
   )
 }
