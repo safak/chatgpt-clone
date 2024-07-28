cb59c00ca1c8a6f059f972c68563829693a9901c
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-07-28 17:35:23 +0530

    commit all

---
+++ client/src/main.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./routes/homepage/Homepage";
+import DashboardPage from "./routes/dashboard/DashboardPage";
+import ChatPage from "./routes/chatPage/ChatPage";
+import RootLayout from "../layouts/RootLayout";
+import DashboardLayout from "../layouts/dashboardLayout/DashboardLayout";
+import SignInPage from "./routes/signInPage/SignInPage";
+import SignUpPage from "./routes/signUpPage/SignUpPage";
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <Homepage /> },
+      { path: "/sign-in/*", element: <SignInPage /> },
+      { path: "/sign-up/*", element: <SignUpPage /> },
+      {
+        element: <DashboardLayout />,
+        children: [
+          { path: "/dashboard", element: <DashboardPage /> },
+          { path: "/dashboard/chats/:id", element: <ChatPage /> },
+        ],
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
