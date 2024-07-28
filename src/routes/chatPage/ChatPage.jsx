98e7dc835a805ff973570d7982351ac7f1f314ff
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-07-28 16:30:54 +0530

    add chat page

--- src/routes/chatPage/ChatPage.jsx
+++ src/routes/chatPage/ChatPage.jsx
@@ -1,7 +1,46 @@
+import { useRef } from "react";
 import "./chatPage.css";
+import { useEffect } from "react";
 
 const ChatPage = () => {
-  return <div>ChatPage</div>;
+  const endRef = useRef(null);
+
+  useEffect(() => {
+    endRef.current.scrollIntoView({ behavior: "smooth" });
+  }, []);
+  return (
+    <div className="chatPage">
+      <div className="wrapper">
+        <div className="chat">
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div ref={endRef}></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChatPage;
