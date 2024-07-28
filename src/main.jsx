c997180f264539d1add954ca635eaa28f17f516b
APayes <apayes@cdvi.ca>
2024-07-28 09:48:44 -0400

    React 19 Auth. with Clerk; 21:17

--- src/main.jsx
+++ src/main.jsx
@@ -6,24 +6,35 @@ import './index.css'
 import HomePage from './routes/homePage/HomePage'
 import DashboardPage from './routes/dashboardPage/DashboardPage'
 import ChatPage from './routes/chatPage/ChatPage'
+import RootLayout from './layouts/rootLayout/RootLayout'
+import DashboardLayout from './layouts/dashboardLayout/DashboardLayout'
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <HomePage />
-  },
-  {
-    path: '/dashboard',    
+    element: <RootLayout />,
     children: [
       {
-        path: '/dashboard',
-        element: <DashboardPage />
+        path: '/',
+        element: <HomePage />,
       },
       {
-        path: '/dashboard/chats/:id',
-        element: <ChatPage />
-      }
-    ]
+        element: <DashboardLayout />,
+        children: [
+          {
+            path: '/dashboard',
+            element: <DashboardPage />,
+          },
+          {
+            path: '/dashboard/chats/:id',
+            element: <ChatPage />,
+          },
+        ],
+      },
+      {
+        path: '/chat',
+        element: <ChatPage />,
+      },
+    ],
   }
 ])
 
