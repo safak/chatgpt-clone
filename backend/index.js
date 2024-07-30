17dcacd840020a49a2cef4e4db7e4f53b4449927
apayesBT <apayes@beyondtechnologies.ca>
2024-07-30 13:15:49 -0400

    package.json files modified with all dependencies from Lama package.json files

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,19 @@
-console.log('test2');
+import express from "express"
+import ImageKit from "imagekit";
+
+const port = process.env.PORT || 3000
+const app = express()
+
+const imagekit = new ImageKit({
+  urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
+  publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGE_KIT_PRIVATE_KEY
+});
+
+app.get('/api/upload', (req, res) => {
+  res.send('it works')
+})
+
+app.listen(port, () => {
+  console.log('Server running on port', port)
+})
