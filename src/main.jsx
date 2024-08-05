8fc2e0a1a5cd11f416f3e24bf48b227d6c81e89b
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2024-08-05 20:35:38 +0100

    installed react-router and implemented routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './routes/homePage/HomePage.jsx';
+import DashBoard from './routes/dashboardPage/DashboardPage.jsx';
+import ChatPage from './routes/chatPage/ChatPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/dashboard',
+    children: [
+      {
+        path: '/dashboard',
+        element: <DashBoard />,
+      },
+      {
+        path: '/dashboard/chats/:id',
+        element: <ChatPage />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
