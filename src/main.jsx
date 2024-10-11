e49bd3080f9a10ab45e63e2cf90447354b754e14
mkpage1337 <mrku1337@gmail.com>
2024-10-11 09:15:24 +0200

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  // Route,
+  // Link,
+} from "react-router-dom";
+import "./index.css";
+import HomePage from "../src/routes/homepage/HomePage.jsx";
+import Dashboard from "../src/routes/dashboardPage/DashboardPage.jsx";
+import ChatPage from "./routes/chatPage/ChatPage";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+    children: [{ path: "/dashboard/chats/:id", element: <ChatPage /> }],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
