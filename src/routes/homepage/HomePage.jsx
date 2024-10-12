64d242241d9a15864402f951ffb797cf3de7a93c
mkpage1337 <mrku1337@gmail.com>
2024-10-12 21:14:19 +0200

    style home page

--- src/routes/homepage/HomePage.jsx
+++ src/routes/homepage/HomePage.jsx
@@ -1,12 +1,29 @@
 import { Link } from "react-router-dom";
 import "./homepage.css";
 
-function Homepage() {
+function HomePage() {
   return (
     <div className="homepage">
-      <Link to="/dashboard">Dashboard</Link>
+      <img src="/orbital.png" alt="" className="orbital" />
+      <div className="left">
+        <h1>LAMA AI</h1>
+        <h2>Supercharge your creativity and productivity</h2>
+        <h3>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat sint
+          dolorem doloribus, architecto dolor.
+        </h3>
+        <Link to="/dashboard">Get Started</Link>
+      </div>
+      <div className="right">
+        <div className="imgContainer">
+          <div className="bgContainer">
+            <div className="bg"></div>
+          </div>
+          <img src="/bot.png" alt="" className="bot" />
+        </div>
+      </div>
     </div>
   );
 }
 
-export default Homepage;
+export default HomePage;
