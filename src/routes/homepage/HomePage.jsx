0a51c3e312cb0f673bfd86b2dc1b7f01126f4ad8
mkpage1337 <mrku1337@gmail.com>
2024-10-13 10:59:22 +0200

    finished homepage

--- src/routes/homepage/HomePage.jsx
+++ src/routes/homepage/HomePage.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import { TypeAnimation } from "react-type-animation";
 import "./homepage.css";
+import { useState } from "react";
 
 function HomePage() {
+  const [typingStatus, setTypingStatus] = useState("human1");
+
   return (
     <div className="homepage">
-      <img src="/orbital.png" alt="" className="orbital" />
+      <img src="/orbital.png" alt="orbital" className="orbital" />
       <div className="left">
         <h1>LAMA AI</h1>
         <h2>Supercharge your creativity and productivity</h2>
@@ -12,14 +16,65 @@ function HomePage() {
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat sint
           dolorem doloribus, architecto dolor.
         </h3>
-        <Link to="/dashboard">Get Started</Link>
+        <Link to="/dashboard" className="btn-start">
+          Get Started
+        </Link>
       </div>
       <div className="right">
         <div className="imgContainer">
           <div className="bgContainer">
             <div className="bg"></div>
           </div>
-          <img src="/bot.png" alt="" className="bot" />
+          <img src="/bot.png" alt="bot" className="bot" />
+          <div className="chat">
+            <img
+              src={
+                typingStatus === "human1"
+                  ? "/human1.jpeg"
+                  : typingStatus === "human2"
+                  ? "/human2.jpeg"
+                  : "/bot.jpg"
+              }
+              alt=""
+              className="chat-img"
+            />
+            <TypeAnimation
+              sequence={[
+                "Human: We produce food for Mice",
+                2000,
+                () => {
+                  setTypingStatus("bot");
+                },
+                "Bot: We produce food for Hamsters",
+                2000,
+                () => {
+                  setTypingStatus("human2");
+                },
+                "Human2: We produce food for Guinea Pigs",
+                2000,
+                () => {
+                  setTypingStatus("bot");
+                },
+                "Bot: We produce food for Chinchillas",
+                2000,
+                () => {
+                  setTypingStatus("human1");
+                },
+              ]}
+              wrapper="span"
+              cursor={true}
+              omitDeletionAnimation={true}
+              repeat={Infinity}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="terms">
+        <img src="/logo.png" alt="logo" className="terms-logo" />
+        <div className="links">
+          <Link to="/">Terms of Service</Link>
+          <span>|</span>
+          <Link to="/">Privacy Policy</Link>
         </div>
       </div>
     </div>
