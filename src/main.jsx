a8ad0f82ecccd0963cbf2b38dab109f006181678
ikesan <ilkerciliv@gmail.com>
2024-10-16 15:11:39 +0100

    Routers added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { Children } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+// import router from "./router"; // Ensure you have this import for the router
+import Homepage from "./routes/homepage/Homepage";
+import ChatPage from "./routes/chatPage/ChatPage";
+import DashboardPage from "./routes/dashboardPage/DashboardPage";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+  {
+    path: "/dashboard",
+    children: [
+      { path: "/dashboard", element: <DashboardPage /> },
+      { path: "/dashboard/chats/:id", element: <ChatPage /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+console.log(`React version: ${React.version}`);
