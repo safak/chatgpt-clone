e4c374d217b128840b0c911b7cccdc081f9d4406
Adim806 <adim806@gmail.com>
2024-10-17 20:21:42 +0300

    first style home page+ animation

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -2,8 +2,25 @@ import './homepage.css'
 import { Link } from 'react-router-dom';
  const Homepage = () => {
   return (
-    <div className='homepage'>
-      <Link to="/dashboard"> Dashboard</Link>
+    <div className="homepage">
+      <img src="/orbital.png" alt="" className="orbital"/>
+
+      <div className="left">
+        <h1>Welcome to Meller AI</h1>
+        <h2>Supercharge your creativity and productivity</h2>
+        <h3>This is the homepage of Meller AI, Enjoy boy..</h3>
+        <Link to="/dashboard">Get Start</Link>
+      </div>
+
+      <div className="right">
+        <div className="imgContainer"> 
+          <div className="bgContainer">
+            <div className="bg"></div>
+
+          </div>
+          <img src="/bot.png" alt="" className="bott" />
+        </div>
+      </div>
     </div>
   )
 }
