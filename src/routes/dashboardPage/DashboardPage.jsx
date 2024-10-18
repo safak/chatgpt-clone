3f2c6a1e614470b87845fba4cbde414d6aefc247
Adim806 <adim806@gmail.com>
2024-10-18 17:35:06 +0300

    Home screen design (chat menu and dashboard)

--- src/routes/dashboardPage/DashboardPage.jsx
+++ src/routes/dashboardPage/DashboardPage.jsx
@@ -1,8 +1,37 @@
-import './dashboardPage.css'
+import './dashboardPage.css';
 
  const DashboardPage = () => {
   return (
-    <div className='DashboardPage'>DashboardPage</div>
+    <div className="DashboardPage">
+      <div className="texts">
+        <div className="logo">
+          <img src="logo.png" alt="" />
+          <h1>MELLER AI</h1>
+        </div>
+        <div className="options">
+          <div className="option">
+            <img src="/chat.png" alt="" />
+            <span>Create a new Chat</span>
+          </div>
+          <div className="option">
+            <img src="/image.png" alt="" />
+            <span>Analyze Images</span>
+          </div>
+          <div className="option">
+            <img src="/code.png" alt="" />
+            <span>Help me with my code</span>
+          </div>
+        </div>
+      </div>
+      <div className="formContainer">
+        <form>
+          <input type="text" placeholder="Ask me Anything..." />
+          <button>
+            <img src="/arrow.png" alt="" />
+          </button>
+        </form>
+      </div>
+    </div>
   );
 };
 
