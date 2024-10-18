5d8ba8e7efa5d1decff9e24c09a924ecb81d8a2d
Adim806 <adim806@gmail.com>
2024-10-18 19:01:07 +0300

    design the chat web (1:17) chat page jsx+css+add useRef HOOK

--- src/routes/chatPage/ChatPage.jsx
+++ src/routes/chatPage/ChatPage.jsx
@@ -1,9 +1,38 @@
-import './chatPage.css'
+import { useRef } from 'react';
+import './chatPage.css';
+import { useEffect } from 'react';
 
  const ChatPage = () => {
+  const endRef= useRef(null);
+  useEffect(() => {
+    const scrollToBottom = () => {
+      endRef.current?.scrollIntoView({ behavior:'smooth' });
+    };
+    scrollToBottom();
+  },[]);
   return (
-    <div className='ChatPage'>ChatPage</div>
-  )
-}
+    <div className="chatPage">
+      <div className="wrapper">
+        <div className="chat">
+          <div className='message'>Test message from ai asdasdas as  heae2 23123 sdasdas</div>
+          <div className='message user'>Test message from user!Test message from user!Test message from user!Test message from user!</div>
+          <div className='message'>Test message from ai</div>
+          <div className='message user'>Test message from user</div>
+          <div className='message'>Test message from ai!Test message from ai!Test message from ai!Test message from ai!!</div>
+          <div className='message user'>Test message from user</div>
+          <div className='message'>Test message from ai</div>
+          <div className='message user'>Test message from user</div>
+          <div className='message'>Test message from ai</div>
+          <div className='message user'>Test message from user</div>
+          <div className='message'>Test message from ai!Test message from ai!Test message from ai!Test message from ai!!</div>
+          <div className='message user'>Test message from user</div>
+          <div className='message'>Test message from ai</div>
+          <div className='message user'>Test message from user</div>
+          <div ref={endRef}/>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ChatPage
+export default ChatPage;
