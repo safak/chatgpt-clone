96801a8a7e6945efad39abb2348089de5ac9a4cf
ikesan <ilkerciliv@gmail.com>
2024-10-20 12:19:34 +0100

    Added Link to dashboard, which redirects to sign in, because were not authenticated.

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
 import "./homepage.css";
 
 const Homepage = () => {
-  return <div className="Homepage">Homepage</div>;
+  return (
+    <div className="Homepage">
+      <Link to="/dashboard">Dashboard</Link>
+    </div>
+  );
 };
 
 export default Homepage;
