61201b1b63d4d3690dd3fcfcda601fd84883f4b2
ikesan <ilkerciliv@gmail.com>
2024-10-20 12:40:07 +0100

    authentication added successfully

--- src/routes/signInPage/SignInPage.jsx
+++ src/routes/signInPage/SignInPage.jsx
@@ -4,7 +4,7 @@ import { SignIn } from "@clerk/clerk-react";
 const SignInPage = () => {
   return (
     <div className="signInPage">
-      <SignIn path="/sign-in" />
+      <SignIn path="/sign-in" signUpUrl="/sign-up" />
     </div>
   );
 };
--- src/routes/signUpPage/SignUpPage.jsx
+++ src/routes/signUpPage/SignUpPage.jsx
@@ -4,7 +4,7 @@ import { SignUp } from "@clerk/clerk-react";
 const SignUpPage = () => {
   return (
     <div className="signUpPage">
-      <SignUp path="/sign-up" />
+      <SignUp path="/sign-up" signInUrl="/sign-in" />
     </div>
   );
 };
