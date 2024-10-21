3bb1930ad009a974f662776cdf309ff15ec4afb5
mkpage1337 <mrku1337@gmail.com>
2024-10-21 22:13:58 +0200

    dashboard refactor

--- src/routes/dashboardPage/DashboardPage.jsx
+++ src/routes/dashboardPage/DashboardPage.jsx
@@ -3,30 +3,31 @@ import "./dashboardPage.css";
 function DashboardPage() {
   return (
     <div className="dashboardPage">
-      <div className="text">
+      <div className="texts">
         <div className="logo">
-          <img src="/logo.png" alt="logo" />
-          <div className="options">
-            <div className="option">
-              <img src="/chat.png" alt="img" />
-              <span>Create a New Chat</span>
-            </div>
-            <div className="option">
-              <img src="/image.png" alt="img" />
-              <span>Analyze Images</span>
-            </div>
-            <div className="option">
-              <img src="/code.png" alt="img" />
-              <span>Help me with my code</span>
-            </div>
+          <img src="/logo.png" alt="" />
+          <h1>LAMA AI</h1>
+        </div>
+        <div className="options">
+          <div className="option">
+            <img src="/chat.png" alt="" />
+            <span>Create a New Chat</span>
+          </div>
+          <div className="option">
+            <img src="/image.png" alt="" />
+            <span>Analyze Images</span>
+          </div>
+          <div className="option">
+            <img src="/code.png" alt="" />
+            <span>Help me with my Code</span>
           </div>
         </div>
       </div>
       <div className="formContainer">
         <form>
-          <input type="text" placeholder="Aske me anything..." />
+          <input type="text" placeholder="Ask me anything..." />
           <button>
-            <img src="/arrow.png" alt="" />
+            <img src="/arrow.png" alt="arrow" />
           </button>
         </form>
       </div>
