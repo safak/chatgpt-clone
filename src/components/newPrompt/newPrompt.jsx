91eb8805e622349ae2d066435294bb9c8370238b
ikesan <ilkerciliv@gmail.com>
2024-10-21 12:02:02 +0100

    NewPrompt component implemented. This component implements the ask bar at the bottom of the chatpage. It is necessary for implementing dynamic rendering for the ChatPageand not refresh page each time a request is made. Scroll functionality moved tp New prompt from ChatPage

--- src/components/newPrompt/newPrompt.jsx
+++ src/components/newPrompt/newPrompt.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useRef } from "react";
 import "./newPrompt.css";
 
 const NewPrompt = () => {
-  return <div className="newPrompt">NewPrompt</div>;
+  const endRef = useRef(null);
+
+  useEffect(() => {
+    endRef.current.scrollIntoView({ behavior: "smooth" });
+  }, []);
+
+  return (
+    <>
+      {/* ADD NEW CHAT */}
+      <div className="endChat"></div>
+      <form className="newForm">
+        <label htmlFor="file">
+          {/* the label is for the file input for uploading images */}
+          <img src="/attachment.png" alt="" />
+        </label>{" "}
+        <input id="file" type="file" multiple={false} hidden />
+        <input type="text" placeholder="Ask me anything..." />
+        <button>
+          <img src="/arrow.png" alt="" />
+        </button>
+      </form>
+      <div ref={endRef}></div>
+    </>
+  );
 };
 
 export default NewPrompt;
--- src/routes/chatPage/ChatPage.jsx
+++ src/routes/chatPage/ChatPage.jsx
@@ -1,14 +1,7 @@
-import { useEffect, useRef } from "react";
 import "./chatPage.css";
-import NewPrompt from "../../components/newPrompt/newPrompt";
+import NewPrompt from "../../components/newPrompt/newPrompt.jsx";
 
 const ChatPage = () => {
-  const endRef = useRef(null);
-
-  useEffect(() => {
-    endRef.current.scrollIntoView({ behavior: "smooth" });
-  }, []);
-
   return (
     <div className="chatPage">
       <div className="wrapper">
@@ -57,7 +50,6 @@ const ChatPage = () => {
           <div className="message">Test message from ai</div>
           <div className="message user">Test message from user</div>
           <NewPrompt />
-          <div ref={endRef}></div>
         </div>
       </div>
     </div>
