1c605eca4afd574dba9e01256499647406fb3d96
mkpage1337 <mrku1337@gmail.com>
2024-10-23 10:41:24 +0200

    commit

--- src/components/chatList/ChatList.jsx
+++ src/components/chatList/ChatList.jsx
@@ -1,29 +1,41 @@
 import { Link } from "react-router-dom";
 import "./chatList.css";
+import { useQuery } from "@tanstack/react-query";
 
 function ChatList() {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["userChats"],
+    queryFn: () =>
+      fetch(`${import.meta.env.VITE_API_URL}/api/userchats`, {
+        credentials: "include",
+      }).then((res) => res.json()),
+  });
+
   return (
     <div className="chatList">
-      <span className="title">Dashboard</span>
-      <Link to="/">Create a new Chat</Link>
+      <span className="title">DASHBOARD</span>
+      <Link to="/dashboard">Create a new Chat</Link>
       <Link to="/">Explore Lama AI</Link>
       <Link to="/">Contact</Link>
       <hr />
+      <span className="title">RECENT CHATS</span>
       <div className="list">
-        <Link to="/">Title chat</Link>
-        <Link to="/">Title chat</Link>
-        <Link to="/">Title chat</Link>
-        <Link to="/">Title chat</Link>
-        <Link to="/">Title chat</Link>
-        <Link to="/">Title chat</Link>
-        <Link to="/">Title chat</Link>
+        {isPending
+          ? "Loading..."
+          : error
+          ? "Something went wrong!"
+          : data?.map((chat) => (
+              <Link to={`/dashboard/chats/${chat._id}`} key={chat._id}>
+                {chat.title}
+              </Link>
+            ))}
       </div>
       <hr />
       <div className="upgrade">
-        <img src="/logo.png" alt="logo" />
+        <img src="/logo.png" alt="" />
         <div className="texts">
-          <span>Upgrate to Pro</span>
-          <span>Unlimited access...</span>
+          <span>Upgrade to Lama AI Pro</span>
+          <span>Get unlimited access to all features</span>
         </div>
       </div>
     </div>
--- src/components/newPrompt/NewPrompt.jsx
+++ src/components/newPrompt/NewPrompt.jsx
@@ -1,7 +1,20 @@
 import "./newPrompt.css";
 
 function NewPrompt() {
-  return <div></div>;
+  return (
+    <div className="newPrompt">
+      <form className="newForm">
+        <label htmlFor="file">
+          <img src="/attachment.png" alt="attachment" />
+        </label>
+        <input id="file" type="file" multiple={false} hidden />
+        <input type="text" placeholder="Ask anything..." />
+        <button>
+          <img src="/arrow.png" alt="arrow btn" />
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default NewPrompt;
