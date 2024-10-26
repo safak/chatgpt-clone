118bfa95991821812b3c33bef1c402b14307167c
mkpage1337 <mrku1337@gmail.com>
2024-10-26 21:18:00 +0200

    upload imagekit

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,13 @@ import express from "express";
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.get("/test", (req, res) => {
+const imagekit = new ImageKit({
+  urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
+  publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGE_KIT_PRIVATE_KEY,
+});
+
+app.get("/api/upload", (req, res) => {
   res.send("it works!");
 });
 
